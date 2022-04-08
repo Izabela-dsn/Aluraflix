8ca861aba92779fa94885db7702add58ef3e2cc0
izabela-dsn <izabeladasilva10@gmail.com>
2022-04-08 16:52:00 -0300

    creating server

---
+++ api/index.js
@@ -0,0 +1,17 @@
+import express from "express"
+import bodyParser from "body-parser"
+
+const app = express()
+// avisa ao express que o body parser vai ser utilizado
+app.use(bodyParser.json())
+const port = 8080
+
+app.get("/teste", (req, res) => {
+  res.status(200).send({ mensagem: "hello, welcome" })
+})
+
+app.listen(port, () => {
+  console.log(`servidor esta rodando na porta ${port}`)
+})
+
+export default app
