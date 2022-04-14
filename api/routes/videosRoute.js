f84bc756e472405f14531cf424fbc9fd7c64fc93
izabela-dsn <izabeladasilva10@gmail.com>
2022-04-14 15:36:57 -0300

    criação das rotas

---
+++ api/routes/videosRoute.js
@@ -0,0 +1,17 @@
+const { Router } = require("express")
+const VideosController = require("../controllers/VideosController")
+
+const router = Router()
+
+// post
+router.post("/videos", VideosController.criaVideo)
+// get
+router.get("/videos", VideosController.pegaTodosOsVideos)
+// get um video
+router.get("/videos/:id", VideosController.pegaUmVideo)
+// delete
+router.delete("/videos/:id", VideosController.deletaVideo)
+// put
+router.put("/videos/:id", VideosController.editaVideo)
+
+module.exports = router
